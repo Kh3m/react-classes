16c8832176dde4b9f3f2e413763d59e8081ec98f
Khem Khem <khem6333@gmail.com>
2024-05-18 12:00:05 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <main>
+      <h1>Welcome to REACT-JS </h1>
+      <Button text="Be a Vendor" />
+
+      <p></p>
+      <Button text="Shop Now" />
+    </main>
+  )
+}
+
+export default App
